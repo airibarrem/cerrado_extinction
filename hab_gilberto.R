c2e336faa451782e2541c90e965e411a65341a80
airibarrem <airibarrem@gmail.com>
2016-04-01 16:05:10 +0200

    Area fixes in BRABIOM analysis
    Areas should be computed consistently this time. Added a plot of the
    native vegetation layers, that should allow for comparison between
    different choices of classes from BRABIOM to be included (namely
    NatLnd).

--- hab_gilberto.R
+++ hab_gilberto.R
@@ -57,52 +57,64 @@ bgd[bgd>0]=0
 # REDD-PAC land-use shapefile
 LU.shp = shapefile('./LU_REDD_PAC/REED_PAC_BAU.shp')
 
+# Reprojecting LU shapefiles to match background of sp.dist rasters
+LU.shp = spTransform(LU.shp,CRS(proj4string(bgd)))
+
 # Names of classes (columns) in LU.shp
 LU.classes = names(LU.shp)
 
+
 # Native vegetation shapefiles (now = 2010, bau = 2050)
-nat.shp.now = LU.shp[LU.classes %in% c('FORwPA2010','NatLnd2010', 'PRIFOR2010',
-                                   'ForReg2010')]
+nat.shp.now = LU.shp[LU.classes %in% c('NatLnd2010','PRIFOR2010','ForReg2010')]
 
-nat.shp.bau = LU.shp[LU.classes %in% c('FORwPA2050','NatLnd2050', 'PRIFOR2050',
-                                   'ForReg2050')]
-#spplot(nat.now[1],z=names(nat.now[1]))
-
-
-# Reprojecting native vegetation maps
-nat.shp.now = spTransform(nat.shp.now,CRS(proj4string(bgd)))
-nat.shp.bau = spTransform(nat.shp.bau,CRS(proj4string(bgd)))
+nat.shp.bau = LU.shp[LU.classes %in% c('NatLnd2050','PRIFOR2050','ForReg2050')]
 
 # Converting native vegetation shapefiles to a combined raster of veg. cover
-nat.now = rasterize(nat.shp.now,bgd,field=nat.shp.now@data[,1])
-nat.now = nat.now + rasterize(nat.shp.now,bgd,field=nat.shp.now@data[,2])
-nat.now = nat.now + rasterize(nat.shp.now,bgd,field=nat.shp.now@data[,3])
-nat.now = nat.now + rasterize(nat.shp.now,bgd,field=nat.shp.now@data[,4])
-nat.now = nat.now + bgd
+nat.shp.now = rasterize(nat.shp.now,bgd)
 
-nat.bau = rasterize(nat.shp.bau,bgd,field=nat.shp.bau@data[,1])
-nat.bau = nat.bau + rasterize(nat.shp.bau,bgd,field=nat.shp.bau@data[,2])
-nat.bau = nat.bau + rasterize(nat.shp.bau,bgd,field=nat.shp.bau@data[,3])
-nat.bau = nat.bau + rasterize(nat.shp.bau,bgd,field=nat.shp.bau@data[,4])
-nat.bau = nat.bau + bgd
+# Each subs command puts in each pixel its corresponding value computed by rasterize
+# Sum over each class in nat.shp corresponding to column in @data@attributes
+nat.now = subs(nat.shp.now,nat.shp.now@data@attributes[[1]][,c(1,2)]) +
+  subs(nat.shp.now,nat.shp.now@data@attributes[[1]][,c(1,3)]) +
+  subs(nat.shp.now,nat.shp.now@data@attributes[[1]][,c(1,4)]) + bgd
+
+# Same procedure for BAU-projection classes
+nat.shp.bau = rasterize(nat.shp.bau,bgd)
+
+nat.bau = subs(nat.shp.bau,nat.shp.bau@data@attributes[[1]][,c(1,2)]) +
+  subs(nat.shp.bau,nat.shp.bau@data@attributes[[1]][,c(1,3)]) +
+  subs(nat.shp.bau,nat.shp.bau@data@attributes[[1]][,c(1,4)]) + bgd
+
+# Converting area_size (in ha) shapefile to a raster, with data in all pixels
+size.map = rasterize(LU.shp[30],bgd)
+size.map = subs(size.map, size.map@data@attributes[[1]][,1:2]) + bgd
+
+# Converting pixel size in native vegetation rasters
+# Multiplicative factor 1000 due to unit differences in original shape file
+# now each pixel has a value of veg. area (ha) corresponding to its own area
+nat.now = (nat.now * 1000) * ((area(bgd) * 100) / size.map)
+nat.bau = (nat.bau * 1000) * ((area(bgd) * 100) / size.map)
 
 
 # Applying intersect.raster and cond.area to all sp.dist files
 sfInit(parallel = T, cpus = 7, type = 'SOCK')
 sfExportAll()
 sfLibrary(raster)
-# sp. potential area maps today
-pot.maps = sfLapply(paste0('./Flora_Cerrado/',sp.files),rcompose,bgd)
+# sp. potential area maps today (each pixel contains its area in ha)
+pot.maps = sfLapply(paste0('./Flora_Cerrado/',sp.files),
+                    function(x){rcompose(x,bgd)*area(bgd)*100})
 # sp. habitats today
-hab.maps = sfLapply(paste0('./Flora_Cerrado/',sp.files),intersect.raster,nat.now,bgd)
+hab.maps = sfLapply(paste0('./Flora_Cerrado/',sp.files),
+                    intersect.raster,nat.now,bgd)
 #sp. habitats under future LUC
-bau.maps = sfLapply(paste0('./Flora_Cerrado/',sp.files),intersect.raster,nat.bau,bgd)
+bau.maps = sfLapply(paste0('./Flora_Cerrado/',sp.files),
+                    intersect.raster,nat.bau,bgd)
 # List of all the maps for today
 maps.list = c(pot.maps,hab.maps,bau.maps)
 # List of the areas (in ha) corresponding to the list of maps above
-pot.areas = sfLapply(pot.maps,function(x){cond.area(nat.now,x)})
-hab.areas = sfLapply(hab.maps,function(x){cond.area(nat.now,x)})
-bau.areas = sfLapply(bau.maps,function(x){cond.area(nat.bau,x)})
+pot.areas = sfLapply(pot.maps,function(x){sum(values(x),na.rm=T)})
+hab.areas = sfLapply(hab.maps,function(x){sum(values(x),na.rm=T)})
+bau.areas = sfLapply(bau.maps,function(x){sum(values(x),na.rm=T)})
 sfStop()
 
 # Computing area data.frames
@@ -122,3 +134,9 @@ write.csv2(now.df, file='Flora_BRABIOM_2')
 
 write.csv(teste.df, file='Flora_BRABIOM_perc')
 write.csv2(teste.df, file='Flora_BRABIOM_perc_2')
+
+# Exporting native vegetation maps
+jpeg("./BRABIOM_natveg.jpg", res=600, width=9, height=9, unit='cm', pointsize=8)
+spplot(stack(nat.now,nat.bau), names.attr=c(2010,2050),
+       col.regions=rainbow(18,start=0.1, end=0.3))
+dev.off()
